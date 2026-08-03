1ef8da1dfcd0d98f4df4329a3feb12349f1a5b14
OmarEbada <omarebada327@gmail.com>
2026-08-03 17:28:12 +0300

    Middleware Folder Completed

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.status || 500;
+    const message = err.message || "Internal Server Error";
+
+    return res.status(statusCode).json({ success: false, message });
+};
+
+module.exports = { errorHandler };
