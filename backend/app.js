779c0c2ee54889ab327fb641d1fc95d4efa8bc5b
OmarEbada <omarebada327@gmail.com>
2026-08-06 20:22:06 +0300

    app file done

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const stationRoutes = require("./routes/stationRoutes");
+const { errorHandler } = require("./middleware/errorHandler");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("api/auth", authRoutes);
+app.use("/api/stations", stationRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ success: false, message: "Route not found"});
+});
+
+app.use(errorHandler);
+
+module.exports = app;
